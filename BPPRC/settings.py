74ad29fbc62725c7f2235a759594746809974f77
Amrithasuresh <sureshcbt@gmail.com>
2020-03-04 14:42:53 -0500

    Fixed allowed hosts name

--- BPPRC/settings.py
+++ BPPRC/settings.py
@@ -32,8 +32,7 @@ if os.environ.get('DEVELOPMENT'):
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'camtech-bpp.ifas.ufl.edu',
-                 'camtech-bpp.test.ifas.ufl.edu', 'www.bpprc.org', 'bpprc.org']
+ALLOWED_HOSTS = ['127.0.0.1', 'camtech-bpp.test.ifas.ufl.edu']
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
