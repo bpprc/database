c2009376aec502b7b8e1e82eee24d474dd9eea82
Amrithasuresh <sureshcbt@gmail.com>
2020-05-08 07:30:03 -0400

    domain analysis added

--- clustalanalysis/forms.py
+++ clustalanalysis/forms.py
@@ -46,8 +46,12 @@ class AnalysisForm(forms.Form):
             self.userdata = [int(s) for s in self.userdata.split(',')]
 
     def clean_session_list_names(self):
-        self.selected_values_1 = ast.literal_eval(
-            self.cleaned_data.get('session_list_names'))
+        try:
+            self.selected_values_1 = ast.literal_eval(
+                self.cleaned_data.get('session_list_names'))
+        except Exception:
+            self.selected_values_1 = []
+        # self.selected_values_1 = self.cleaned_data['session_list_names']
 
         if not self.selected_values_1:
             # HiddenInput does not produce any validation error how to fix this
@@ -57,21 +61,62 @@ class AnalysisForm(forms.Form):
                 "Select atleast more than two sequences")
         return self.selected_values_1
 
+    # def clean_session_list_nterminal(self):
+    #     self.list_nterminal = ast.literal_eval(
+    #         self.cleaned_data.get('session_list_nterminal'))
+    #
+    # def clean_session_list_cterminal(self):
+    #     self.list_cterminal = ast.literal_eval(
+    #         self.cleaned_data.get('session_list_cterminal'))
+    #
+    # def clean_session_list_middle(self):
+    #     self.list_middle = ast.literal_eval(
+    #         self.cleaned_data.get('session_list_middle'))
+
     def clean(self):
-        self.list_nterminal = self.cleaned_data['session_list_nterminal']
-        self.list_cterminal = self.cleaned_data['session_list_cterminal']
-        self.list_middle = self.cleaned_data['session_list_middle']
-        # print("The nterminal: ", self.list_nterminal)
-        # print("The middle domain:", self.list_middle)
-        # print("The cterminal domain", self.list_cterminal)
+        try:
+            self.list_nterminal = ast.literal_eval(
+                self.cleaned_data.get('session_list_nterminal'))
+        except:
+            self.list_nterminal = []
+
+        try:
+            self.list_cterminal = ast.literal_eval(
+                self.cleaned_data.get('session_list_cterminal'))
+        except:
+            self.list_cterminal = []
+
+        try:
+            self.list_middle = ast.literal_eval(
+                self.cleaned_data.get('session_list_middle'))
+        except:
+            self.list_middle = []
+        # self.list_nterminal = self.cleaned_data['session_list_nterminal']
+        # self.list_cterminal = self.cleaned_data['session_list_cterminal']
+        # self.list_middle = self.cleaned_data['session_list_middle']
+
+        # print("type")
+
+        # combine all selection
+        self.combined_selection = self.list_nterminal + \
+            self.list_cterminal + self.list_middle + self.selected_values_1
+        # print("type", type(self.combined_selection))
+        # print("type", self.combined_selection)
+
+        # print("values", self.combined_selection)
+        # print("type", type(self.combined_selection))
 
     def save(self):
-        print('i am save function')
         self.write_files_for_clustal()
         self.protein_detail_data()
         self.write_input_file_clustal()
+        self.count_number_lines()
 
-        return self.clustalomega_in_tmp.name, self.guidetree_out_tmp.name
+        return self.clustalomega_in_tmp.name, self.guidetree_out_tmp.name, self.num_lines
+
+    def count_number_lines(self):
+        self.num_lines = sum(1 for line in open(
+            self.clustalomega_in_tmp.name) if line.startswith(">"))
 
     def write_files_for_clustal(self):
         """ Validate protein sequence """
@@ -84,78 +129,53 @@ class AnalysisForm(forms.Form):
 
     def protein_detail_data(self):
         self.accession = {}
-        # self.filtered_cry = list(
-        #     filter(lambda x: x[0:3] == 'Cry', self.selected_values_1))
-        # self.unique_cry = [
-        #     elem for elem in self.filtered_cry if elem not in self.list_nterminal]
 
-        # self.common_cry = lists(set(self.filtered_cry)).intersection(self.list_nterminal)
-        #
-        # self.filtered_non_cry = list(
-        #     filter(lambda x: x[0:3] != 'Cry', self.selected_values_1))
         self.data = \
             PesticidalProteinDatabase.objects.filter(
-                name__in=self.selected_values_1)
+                name__in=self.combined_selection)
         if self.data:
             for item in self.data:
                 self.accession[item.accession] = item
 
         self.protein_detail = ProteinDetail.objects.filter(
             accession__in=list(self.accession.keys()))
+        # print(self.data)
 
     def write_input_file_clustal(self):
-        print('i am write file for clustal function')
-        # data = \
-        #     PesticidalProteinDatabase.objects.filter(
-        #         name__in=self.selected_values_1)
-
-        # unique_cry = \
-        #     PesticidalProteinDatabase.objects.filter(
-        #         name__in=self.unique_cry)
-        #
-        # cry = \
-        #     PesticidalProteinDatabase.objects.filter(
-        #         name__in=self.filtered_cry)
-        # unique_nterminal = \
-        #     PesticidalProteinDatabase.objects.filter(
-        #         name__in=self.list_nterminal)
-        #
-        # unique_middle = \
-        #     PesticidalProteinDatabase.objects.filter(
-        #         name__in=self.list_middle)
-        #
-        # unique_cterminal = \
-        #     PesticidalProteinDatabase.objects.filter(
-        #         name__in=self.list_cterminal)
-
         userdata = UserUploadData.objects.filter(
             pk__in=self.userdata)
 
         with open(self.clustalomega_in_tmp.name, 'wb') as temp:
-            # for protein in cry:
-            # output = ''
-            # if protein in self.list_nterminal:
-            #     output += protein.get_endotoxin_n()
-
-            # k = [item for item in self.data if item.name in self.list_nterminal]
-
             for item in self.data:
-                # output = ''
-                # if item.name in self.list_nterminal:
-                #     nterminal = [
-                #         protein for protein in self.protein_detail if protein.accession == item.accession]
-                #     for item1 in nterminal:
-                #         try:
-                #             output += item1.get_endotoxin_n()
-                #             # output = textwrap.fill(output, 80)
-                #         except:
-                #             print("item", item)
-                #             pass
-                #
-                # else:
-                fasta = textwrap.fill(item.sequence, 80)
-                str_to_write = f">{item.name}\n{fasta}\n"
-                temp.write(str_to_write.encode())
+                output = ''
+                if item.name in self.list_nterminal:
+                    nterminal = [
+                        protein for protein in self.protein_detail if protein.accession == item.accession]
+                    for item1 in nterminal:
+                        output += item1.get_endotoxin_n()
+                if item.name in self.list_cterminal:
+                    cterminal = [
+                        protein for protein in self.protein_detail if protein.accession == item.accession]
+                    for item1 in cterminal:
+                        output += item1.get_endotoxin_c()
+                if item.name in self.list_middle:
+                    # print('middle loop running')
+                    # print(item.name)
+                    middle = [
+                        protein for protein in self.protein_detail if protein.accession == item.accession]
+                    for item1 in middle:
+                        output += item1.get_endotoxin_m()
+                else:
+                    # print("item", item)
+                    fasta = textwrap.fill(item.sequence, 80)
+                    str_to_write = f">{item.name}\n{fasta}\n"
+                    temp.write(str_to_write.encode())
+
+                if output:
+                    # print('middle domain')
+                    # print(output)
+                    str_to_write = f">{item.name}\n{output}\n"
+                    temp.write(str_to_write.encode())
 
             for item in userdata:
                 fasta = textwrap.fill(item.sequence, 80)
@@ -168,7 +188,7 @@ class DendogramForm(forms.Form):
     category_type = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
         choices='',
-        required=True
+        required=False
     )
 
     def __init__(self, *args, **kwargs):
@@ -193,14 +213,19 @@ class DendogramForm(forms.Form):
             sorted(self.category_prefixes.items(), key=lambda x: x[0][:3]))
 
         self.fields['category_type'].choices = self.category_options
-        self.fields['category_type'].label = 'Category types'
+        self.fields['category_type'].label = 'Category Types'
 
     def save(self):
         self.open_files_for_clustal()
         self.filter_categories()
         self.write_input_file_clustal()
+        self.count_number_lines()
         # self.run_clustal()
-        return self.clustalomega_in_tmp.name, self.guidetree_out_tmp.name, self.newlines
+        return self.clustalomega_in_tmp.name, self.guidetree_out_tmp.name, self.num_lines
+
+    def count_number_lines(self):
+        self.num_lines = sum(1 for line in open(
+            self.clustalomega_in_tmp.name) if line.startswith(">"))
 
     def open_files_for_clustal(self):
         """ open files for clustal """
@@ -226,14 +251,12 @@ class DendogramForm(forms.Form):
     def write_input_file_clustal(self):
         """ """
         str_to_write = b''
-        self.newlines = ''
         with open(self.clustalomega_in_tmp.name, 'wb') as temp:
             for category in self.category_type:
                 if category == 'all':
                     for item in self.data:
                         str_to_write = f">{item.name}\n{item.sequence}\n"
                         lines = str_to_write.count('\n')
-                        self.newlines = lines
                         temp.write(str_to_write.encode())
                 else:
                     for item in self.data:
@@ -241,5 +264,4 @@ class DendogramForm(forms.Form):
                             if category.capitalize() in item.name:
                                 str_to_write = f">{item.name}\n{item.sequence}\n"
                                 lines = str_to_write.count('\n')
-                                self.newlines = lines
                                 temp.write(str_to_write.encode())
