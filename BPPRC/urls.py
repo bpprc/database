2e06ac8d8c2ed188133b076e5f8ca6882ac54a79
Amrithasuresh <sureshcbt@gmail.com>
2020-08-19 00:24:11 -0400

    admin password reset added

--- BPPRC/urls.py
+++ BPPRC/urls.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
 from django.conf.urls.static import static, settings
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 handler404 = 'database.views.page_not_found'
 handler500 = 'database.views.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'admin/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'admin/reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('database.urls')),
     path('', include('bestmatchfinder.urls')),
     path('', include('namingalgorithm.urls')),
