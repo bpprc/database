6226effe20c933fe4eecd839283558499341b793
amrithasuresh <sureshcbt@gmail.com>
2021-04-03 00:23:20 -0400

    api app removed

--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
-
-
-class ApiConfig(AppConfig):
-    name = 'api'
+# from django.apps import AppConfig
+#
+#
+# class ApiConfig(AppConfig):
+#     name = 'api'
--- api/views.py
+++ api/views.py
@@ -1,25 +1,25 @@
-from django.shortcuts import render
-from api.forms import NeedleAPIForm
-from database.models import PesticidalProteinDatabase
-# from api import emboss_needle
-
-
-def needle_api(request):
-    # use 127.0.0.1:8000/home_api
-    response = requests.get('http://ip-api.com/json/128.227.118.11')
-    geodata = response.json()
-    return render(request, 'association/home_api.html', {
-        'ip': geodata
-    })
-
-
-def needle_api_form(request):
-    form = NeedleAPIForm()
-    return render(request, 'association/best_match_finder_database.html', {'form': form})
-
-
-def needle_api_run(request):
-    """ This runs bestmatchfinder from the database."""
-    if request.method == 'POST':
-        form = NeedleAPIForm(request.POST)
-        print(form)
+# from django.shortcuts import render
+# from api.forms import NeedleAPIForm
+# from database.models import PesticidalProteinDatabase
+# # from api import emboss_needle
+#
+#
+# def needle_api(request):
+#     # use 127.0.0.1:8000/home_api
+#     response = requests.get('http://ip-api.com/json/128.227.118.11')
+#     geodata = response.json()
+#     return render(request, 'association/home_api.html', {
+#         'ip': geodata
+#     })
+#
+#
+# def needle_api_form(request):
+#     form = NeedleAPIForm()
+#     return render(request, 'association/best_match_finder_database.html', {'form': form})
+#
+#
+# def needle_api_run(request):
+#     """ This runs bestmatchfinder from the database."""
+#     if request.method == 'POST':
+#         form = NeedleAPIForm(request.POST)
+#         print(form)
