9a8a95e45b3d1434aa3210782a96a1a259cf33c5
amrithasuresh <sureshcbt@gmail.com>
2021-05-12 11:31:14 -0400

    format html url link added

--- extra/admin.py
+++ extra/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
 from extra.models import Feedback, Links
+from django.utils.html import format_html
 
 # Register your models here.
 
@@ -15,10 +16,13 @@ class LinksResource(resources.ModelResource):
 class LinksAdmin(ImportExportModelAdmin):
     resource_class = LinksResource
 
-    list_display = ('name', 'description', 'link',)
+    list_display = ('name', 'description', 'link_url')
 
     def __str__(self):
         return self.name
+
+    def link_url(self, obj):
+        return format_html('<a href="%s" target="_blank">%s</a>' % (obj.link, obj.link))
 
 
 class FeedbackResource(resources.ModelResource):
