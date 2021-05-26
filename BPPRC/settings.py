0223afed695f896ab8aa72089d95cd0d94b585ef
amrithasuresh <sureshcbt@gmail.com>
2021-05-26 11:31:54 -0400

    replaced postgres with sqlite for testing

--- BPPRC/settings.py
+++ BPPRC/settings.py
@@ -47,17 +47,24 @@ if os.environ.get('DATABASE_TYPE') == 'sqlite3':
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-if os.environ.get('GITHUB_WORKFLOW'):
+if sys.argv[1] == 'test':
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'github_actions',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+# if os.environ.get('GITHUB_WORKFLOW'):
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'github_actions',
+#             'USER': 'postgres',
+#             'PASSWORD': 'postgres',
+#             'HOST': '127.0.0.1',
+#             'PORT': '5432',
+#         }
+#     }
 else:
     DATABASES = {
         'default': {
