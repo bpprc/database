b7bd85938756a7df0b4c88ab85dadc5c747c7a41
amrithasuresh <sureshcbt@gmail.com>
2021-05-26 11:53:29 -0400

    fixed a syntax error

--- BPPRC/settings.py
+++ BPPRC/settings.py
@@ -48,24 +48,13 @@ if os.environ.get('DATABASE_TYPE') == 'sqlite3':
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-if sys.argv[1] == 'test':
+elif sys.argv[1] == 'test':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-# if os.environ.get('GITHUB_WORKFLOW'):
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'github_actions',
-#             'USER': 'postgres',
-#             'PASSWORD': 'postgres',
-#             'HOST': '127.0.0.1',
-#             'PORT': '5432',
-#         }
-#     }
 else:
     DATABASES = {
         'default': {
