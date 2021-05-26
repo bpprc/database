e8c82c1e51e00a494de3f7c7d486d24228c70058
amrithasuresh <sureshcbt@gmail.com>
2021-05-26 10:40:15 -0400

    added postgres in gitub actions - github_workflow

--- BPPRC/settings.py
+++ BPPRC/settings.py
@@ -47,6 +47,17 @@ if os.environ.get('DATABASE_TYPE') == 'sqlite3':
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
 else:
     DATABASES = {
         'default': {
