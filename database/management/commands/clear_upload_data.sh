544c32ebad8151e7b08f86173916f76a9a511acc
amrithasuresh <sureshcbt@gmail.com>
2021-05-30 19:14:43 -0400

    renamed files - data upload commands

--- database/management/commands/clear_upload_data.sh
+++ database/management/commands/clear_upload_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-python manage.py clear_models
-python manage.py upload_association
-python manage.py upload_data_hidden_sequence_database
-#python manage.py upload_data_private_database needs csv input file
-python manage.py upload_data_public_database
-python manage.py upload_data_structure_database
-python manage.py upload_description
-python manage.py upload_newname
-python manage.py upload_oldname
+python manage.py delete_all_models
+python manage.py upload_association_database
+python manage.py upload_hidden_sequence_database
+#python manage.py upload_private_database needs csv input file
+python manage.py upload_public_database
+python manage.py upload_structure_database
+python manage.py upload_description_database
+python manage.py upload_newname_database
+python manage.py upload_oldname_database
 python manage.py upload_threedomain_data
