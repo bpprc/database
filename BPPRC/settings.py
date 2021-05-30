1b42fedc31c3160de3896b7ee5652b59cbf43447
amrithasuresh <sureshcbt@gmail.com>
2021-05-30 08:02:11 -0400

    removed irritating logfile

--- BPPRC/settings.py
+++ BPPRC/settings.py
@@ -216,52 +216,52 @@ NEEDLE_PATH = os.environ.get("NEEDLE_PATH", "")
 BLAST_PATH = os.environ.get("BLAST_PATH", "")
 CLUSTAL_PATH = os.environ.get("CLUSTAL_PATH", "")
 
-# LOGGING
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "formatters": {
-        "standard": {
-            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            "datefmt": "%d/%b/%Y %H:%M:%S",
-        },
-    },
-    "handlers": {
-        "null": {
-            "level": "DEBUG",
-            "class": "logging.NullHandler",
-        },
-        "logfile": {
-            "level": "DEBUG",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": BASE_DIR + "/logs/djangologfile.log",
-            "maxBytes": 50000,
-            "backupCount": 2,
-            "formatter": "standard",
-        },
-        "console": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-            "formatter": "standard",
-        },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["console"],
-            "propagate": True,
-            "level": "WARN",
-        },
-        "django.db.backends": {
-            "handlers": ["console"],
-            "level": "DEBUG",
-            "propagate": False,
-        },
-        "bestmatchfinder": {
-            "handlers": ["console", "logfile"],
-            "level": "DEBUG",
-        },
-    },
-}
+# # LOGGING
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": True,
+#     "formatters": {
+#         "standard": {
+#             "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+#             "datefmt": "%d/%b/%Y %H:%M:%S",
+#         },
+#     },
+#     "handlers": {
+#         "null": {
+#             "level": "DEBUG",
+#             "class": "logging.NullHandler",
+#         },
+#         "logfile": {
+#             "level": "DEBUG",
+#             "class": "logging.handlers.RotatingFileHandler",
+#             "filename": BASE_DIR + "/logs/djangologfile.log",
+#             "maxBytes": 50000,
+#             "backupCount": 2,
+#             "formatter": "standard",
+#         },
+#         "console": {
+#             "level": "INFO",
+#             "class": "logging.StreamHandler",
+#             "formatter": "standard",
+#         },
+#     },
+#     "loggers": {
+#         "django": {
+#             "handlers": ["console"],
+#             "propagate": True,
+#             "level": "WARN",
+#         },
+#         "django.db.backends": {
+#             "handlers": ["console"],
+#             "level": "DEBUG",
+#             "propagate": False,
+#         },
+#         "bestmatchfinder": {
+#             "handlers": ["console", "logfile"],
+#             "level": "DEBUG",
+#         },
+#     },
+# }
 
 ADMIN_REORDER = [
     {
