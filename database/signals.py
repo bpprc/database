756d780dc83db5f5b776e76286a327350b4d39d5
amrithasuresh <sureshcbt@gmail.com>
2021-05-31 20:17:32 -0400

    hidden sequence added

--- database/signals.py
+++ database/signals.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from database.models import PesticidalProteinDatabase
+from database.models import PesticidalProteinHiddenSequence
 
 
 def filter_files_ending_with_one(SUBJECT_FASTAFILES):
@@ -17,7 +18,17 @@ def filter_files_ending_with_one(SUBJECT_FASTAFILES):
 
 def write_files(objectname):
     path = Path(settings.MEDIA_ROOT, "run_bestmatchfinder/")
-    print("path", path)
+    for protein in objectname:
+        file = protein.name + ".txt"
+        name = str(path) + "/" + file
+        with open(name, "wb") as file:
+            fasta = textwrap.fill(protein.sequence, 80)
+            str_to_write = f">{protein.name}\n{fasta}\n"
+            file.write(str_to_write.encode())
+
+
+def write_files_naming(objectname):
+    path = Path(settings.MEDIA_ROOT, "search_namingalgorithm/")
     for protein in objectname:
         file = protein.name + ".txt"
         name = str(path) + "/" + file
@@ -28,16 +39,27 @@ def write_files(objectname):
 
 
 @receiver(post_save, sender=PesticidalProteinDatabase)
-def update_files(sender, instance, **kwargs):
+def update_bestmatch_files(sender, instance, **kwargs):
     proteins = PesticidalProteinDatabase.objects.values_list(
         "name", flat=True)
     protein_endwith1 = filter_files_ending_with_one(list(proteins))
-
-    # private_proteins = PesticidalProteinPrivateDatabase.objects.values_list(
-    #     'name', flat=True)
     proteins_filtered = PesticidalProteinDatabase.objects.filter(
         name__in=protein_endwith1)
     write_files(proteins_filtered)
 
 
-#post_save.connect(update_files, sender=PesticidalProteinDatabase)
+@receiver(post_save, sender=PesticidalProteinDatabase)
+def update_naming_files(sender, instance, **kwargs):
+    proteins = PesticidalProteinDatabase.objects.values_list(
+        "name", flat=True)
+    protein_endwith1 = filter_files_ending_with_one(list(proteins))
+    proteins_filtered = PesticidalProteinDatabase.objects.filter(
+        name__in=protein_endwith1)
+    write_files_naming(proteins_filtered)
+
+
+# @receiver(post_save, sender=PesticidalProteinHiddenSequence)
+# def update_hidden_files(sender, instance, **kwargs):
+#     proteins = PesticidalProteinDatabase.objects.values_list(
+#         "name", flat=True)
+#     write_files_naming(proteins)
