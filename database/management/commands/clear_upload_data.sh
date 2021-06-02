2f9ca30bd17576d17bea87dc8159243b1458c1b1
amrithasuresh <sureshcbt@gmail.com>
2021-06-02 15:38:37 -0400

    ignorenonexistent option tips added

--- database/management/commands/clear_upload_data.sh
+++ database/management/commands/clear_upload_data.sh
@@ -10,6 +10,13 @@ python manage.py upload_newname_database
 python manage.py upload_oldname_database
 python manage.py upload_threedomain_data
 
-#https://pythoneatstail.com/en/overview-all-articles/backup-and-restore-django-site/
+# https://pythoneatstail.com/en/overview-all-articles/backup-and-restore-django-site/
 python3 manage.py dbbackup
 python3 manage.py mediabackup
+
+# if you remove a feild and want to load data use --ignorenonexistent
+python manage.py loaddata /Users/suresh/Desktop/May_21.json -ignorenonexistent
+
+
+# Automatically change the .env file in git
+https://stackoverflow.com/questions/62841470/how-to-have-branch-specific-variables-in-git-and-python
